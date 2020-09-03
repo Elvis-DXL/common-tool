926b03f8bd948f347953b38614b02cca37132021
Elvis-DXL <dxiaolong@outlook.com>
2020-09-03 11:44:24 +0800

    完成邮件工具

--- src/main/java/com/elvis/common/prop/EmailProp.java
+++ src/main/java/com/elvis/common/prop/EmailProp.java
@@ -1,6 +1,8 @@
 package com.elvis.common.prop;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.ToString;
 
 import java.io.Serializable;
 
@@ -9,6 +11,18 @@ import java.io.Serializable;
  * @since : 2020/8/28 17:37
  */
 @Data
+@ToString
 public class EmailProp implements Serializable {
-
+    @ApiModelProperty("服务器")
+    private String host;
+    @ApiModelProperty("端口")
+    private String port;
+    @ApiModelProperty("协议")
+    private String protocol;
+    @ApiModelProperty("发送人邮件地址")
+    private String userName;
+    @ApiModelProperty("发送人邮件密码/授权码")
+    private String password;
+    @ApiModelProperty("邮件发送人名称")
+    private String name;
 }
--- src/main/java/com/elvis/common/util/EmailUtil.java
+++ src/main/java/com/elvis/common/util/EmailUtil.java
@@ -1,6 +1,8 @@
 package com.elvis.common.util;
 
 import com.elvis.common.prop.EmailProp;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 
 /**
  * @author : Elvis
@@ -19,6 +21,11 @@ public class EmailUtil {
         if (null == emailUtil) {
             synchronized (EmailUtil.class) {
                 if (null == emailUtil) {
+                    if (emailProp == null || StringUtil.isEmpty(emailProp.getUserName()) || StringUtil.isEmpty(emailProp.getPassword())
+                            || StringUtil.isEmpty(emailProp.getProtocol()) || StringUtil.isEmpty(emailProp.getHost())
+                            || StringUtil.isEmpty(emailProp.getPort())) {
+                        throw new IllegalArgumentException("Configuration information error[" + emailProp.toString() + "]");
+                    }
                     emailUtil = new EmailUtil(emailProp);
                 }
             }
@@ -26,5 +33,28 @@ public class EmailUtil {
         return emailUtil;
     }
 
+    /**
+     * 发送邮件
+     *
+     * @param title       标题
+     * @param msg         内容
+     * @param targetEmail 目标邮箱地址
+     */
+    public void sendEmail(String title, String msg, String... targetEmail) throws EmailException {
+        if (StringUtil.isEmpty(title) || StringUtil.isEmpty(msg) || null == targetEmail || targetEmail.length == 0) {
+            throw new IllegalArgumentException("Missing parameters[title=" + title + ";msg=" + msg + ";targetEmail=" + targetEmail);
+        }
+        SimpleEmail simpleEmail = new SimpleEmail();
+        simpleEmail.setSSLOnConnect(true);
+        simpleEmail.setHostName(emailProp.getHost());
+        simpleEmail.setSslSmtpPort(emailProp.getPort());
+        simpleEmail.setAuthentication(emailProp.getUserName(), emailProp.getPassword());
+        simpleEmail.setFrom(emailProp.getUserName(), emailProp.getName());
+        simpleEmail.setCharset("UTF-8");
+        simpleEmail.addTo(targetEmail);
+        simpleEmail.setSubject(title);
+        simpleEmail.setMsg(msg);
+        simpleEmail.send();
+    }
 
 }
